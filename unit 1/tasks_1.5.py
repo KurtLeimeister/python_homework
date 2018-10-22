d37272b9e429f5d6fb2d13b3b4e014fe8725b598
Kurt Leimeister <kurt.leimeister@gmail.com>
2018-10-22 23:47:29 +0300

    added practice .ipynb

---
+++ unit 1/tasks_1.5.py
@@ -0,0 +1,41 @@
+#Python academy
+#Unit 1
+#module 1.1
+#1.5 / ASCII Art
+
+
+# print an ASCII bird
+# looks good only with monospace fonts
+print("")
+print("Task 1: - create the flying bird in character art in the Code cell below")
+print("")
+print("The bird is the word")
+print("_         _      ")
+print(" \       /     ")
+print("  \ . . /   ")
+print("     V   ")
+print("")
+
+
+# [ ] capital letter "E" character art
+print("Task 2: print a capital letter E in character art")
+# generated via http://patorjk.com/software/taag/#p=display&f=Doh&t=E
+print("")
+print("EEEEEEEEEEEEEEEEEEEEEE")
+print("E::::::::::::::::::::E")
+print("E::::::::::::::::::::E")
+print("EE::::::EEEEEEEEE::::E")
+print("E:::::E       EEEEEE")
+print("E:::::E             ")
+print("E::::::EEEEEEEEEE   ")
+print("E:::::::::::::::E   ")
+print("E:::::::::::::::E   ")
+print("E::::::EEEEEEEEEE   ")
+print("E:::::E             ")
+print("E:::::E       EEEEEE")
+print("EE::::::EEEEEEEE:::::E")
+print("E::::::::::::::::::::E")
+print("E::::::::::::::::::::E")
+print("EEEEEEEEEEEEEEEEEEEEEE")
+print("")
+# looks shit without monospace fonts but ok in terminal
