2ec16bb616d3ee266174d99db18c99335dd1e05e
Kurt Leimeister <kurt.leimeister@gmail.com>
2018-10-28 21:34:28 +0200

    adjust folder structure to reflect exercises. I.e. files previously saved in unit 2 actually belong to unit 1 module 1.2

---
+++ unit 1/module_2/tasks_1.3-1.py
@@ -0,0 +1,23 @@
+# Python academy, unit 1, module 2
+# By Kurt, 2018-10-28
+
+# Task 1: PRINT MORE THINGS
+#[ ] increase the number of arguments used in print() to 8 or more
+student_age = 17
+student_name = "Hiroto Yamaguchi"
+print(student_name,'will be in the class for',student_age, 'year old students.','5','6','7','8')
+
+# Task 2: Capslock printing 1
+# [ ] define (def) a simple function called yell_it() and call the function
+phrase = 'Indentation matters'
+
+
+# defining function
+# input: none
+# output: none
+
+def yell_it():
+    print(phrase.upper() + '!')
+
+
+yell_it()
